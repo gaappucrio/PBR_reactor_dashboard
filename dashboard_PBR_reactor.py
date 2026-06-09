c6347071fb6e4cace59b5e345aa5307e1f9ae5a5
Luanchdp <luan.13chagas@gmail.com>
2026-06-09 10:42:39 -0300

    Update dashboard_PBR_reactor.py

--- dashboard_PBR_reactor.py
+++ dashboard_PBR_reactor.py
@@ -64,26 +64,13 @@ with tab1:
     """, unsafe_allow_html=True)
 
     st.latex(r"F_{A0} - F_A + G_A = \frac{dN_A}{dt}  \ \ \ \ \ \ \ \ \ \    (Eq. 1)")
-    st.markdown(
-    r"""
-    <span style='font-size:20px'>
-    Onde $F_{A0}$ é a vazão molar inicial de entrada, $F_A$ é a vazão molar de saída, 
-    $G_A$ é a taxa de geração interna do componente e $\frac{dN_A}{dt}$ representa o 
-    acúmulo de matéria no sistema ao longo do tempo.
-    <br><br>
-    Sob a condição de estado estacionário (steady-state), as propriedades do sistema 
-    tornam-se invariáveis em relação ao tempo, o que anula o termo de acúmulo 
-    ($\frac{dN_A}{dt} = 0$), reduzindo a expressão para:
-    </span>
-    """, 
-    unsafe_allow_html=True
-)
-    #st.write(r"""
-    #<p style='font-size:20px'>
-    #Onde *$F_{A0}$* é a vazão molar inicial de entrada, *$F_A$* é a vazão molar de saída, *$G_A$* é a taxa de geração interna do componente e $dN_A/dt$ representa o acúmulo de matéria no sistema ao longo do tempo.
-    #Sob a condição de estado estacionário (steady-state), as propriedades do sistema tornam-se invariáveis em relação ao tempo, o que anula o termo de acúmulo ($\frac{dN_A}{dt} = 0$), reduzindo a expressão para:
-    #</p>
-    #""", unsafe_allow_html=True)
+  
+    st.write(r"""
+    <p style='font-size:20px'>
+    Onde *$F_{A0}$* é a vazão molar inicial de entrada, *$F_A$* é a vazão molar de saída, *$G_A$* é a taxa de geração interna do componente e $dN_A/dt$ representa o acúmulo de matéria no sistema ao longo do tempo.
+    Sob a condição de estado estacionário (steady-state), as propriedades do sistema tornam-se invariáveis em relação ao tempo, o que anula o termo de acúmulo ($\frac{dN_A}{dt} = 0$), reduzindo a expressão para:
+    </p>
+    """, unsafe_allow_html=True)
     # st.image("https://github.com/amandalemette/ENG1818/blob/6fb679e023faf5918633c3fd921cb7b46d914e29/Imagens/im6.png?raw=true", caption="Esquema simplificado de um reator PBR")
     
     st.header("O Modelo")
